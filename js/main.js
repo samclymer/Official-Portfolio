ad4724005ba91792f975e487dbcb59425f39d4c8
Sam C <samclymer@gmail.com>
2019-02-28 14:32:24 -0500

    beginning rebuild of new portfolio.  Created bottom nav bar with fade in.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/* Fade in Navigation Icons upon page load*/
+$(document).ready(function(){
+      $("#navIcon1").fadeIn(2000);
+      $("#navIcon2").fadeIn(2500);
+      $("#navIcon3").fadeIn(3000);
+      $("#navIcon4").fadeIn(3500);
+      $("#navIcon5").fadeIn(4000);
+  });
+
