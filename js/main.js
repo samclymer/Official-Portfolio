6dce82ebcde99766d9100c4a8501dd0855fc1943
Sam C <samclymer@gmail.com>
2019-03-01 10:50:58 -0500

    new fade in for all elements

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 /* Fade in Navigation Icons upon page load*/
 $(document).ready(function(){
-      $("#bgvid").fadeIn(3000);
+      $("#bgvid").fadeIn(2000);
       $("#navIcon1").fadeIn(2000);
       $("#navIcon2").fadeIn(2500);
       $("#navIcon3").fadeIn(3000);
       $("#navIcon4").fadeIn(3500);
       $("#navIcon5").fadeIn(4000);
+      $(".fontTitle").fadeIn(3000);
+      $(".fontSubTitle").fadeIn(3500);
+      $(".headshot").fadeIn(4000);
   });
 
