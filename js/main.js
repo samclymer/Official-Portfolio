c940aa11617c7086e056ae3c712a761ec3eb0844
Sam C <samclymer@gmail.com>
2019-03-06 16:22:46 -0500

    resume page added with animations

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
     $(".fontTitle").animate({marginLeft: "0px"}, 3000);
     $(".fontSubTitleLeft").animate({marginLeft: "0px"}, 3000);
     $(".fontSubTitleBody").animate({marginTop: "10px"}, 3000);
+    $(".pdf").animate({marginTop: "10px"}, 3000);
     $(".fontRegular").animate({marginTop: "0px"}, 3000);
     $(".headshot").animate({marginLeft: "0px"}, 3000);
     $(".skillsIcon").animate({marginTop: "0px"}, 3000);
@@ -27,6 +28,7 @@ $(document).ready(function(){
     $(".fontTitle").animate({marginLeft: "-1000px"}, 3000);
     $(".fontSubTitleLeft").animate({marginLeft: "-1000px"}, 3000);
     $(".fontSubTitleBody").animate({marginTop: "2000px"}, 3000);
+    $(".pdf").animate({marginTop: "2000px"}, 3000);
     $(".fontRegular").animate({marginTop: "2000px"}, 3000);
     $(".headshot").animate({marginLeft: "-1000px"}, 3000);
     $(".skillsIcon").animate({marginTop: "2000px"}, 3000);
@@ -37,7 +39,74 @@ $(document).ready(function(){
     $("#socialIcon4").animate({marginLeft: "-1000px"}, 3000);
     setTimeout(function(){
         window.location = "./index.html"
-    }, 3500);
+    }, 3000);
   });
 
+  $(navIcon2).click(function(){
+    $("#bgvid").fadeOut(3000);
+    $("#navIcon1").fadeOut(3000);
+    $("#navIcon2").fadeOut(3000);
+    $("#navIcon3").fadeOut(3000);
+    $("#navIcon4").fadeOut(3000);
+    $(".fontTitle").animate({marginLeft: "-1000px"}, 3000);
+    $(".fontSubTitleLeft").animate({marginLeft: "-1000px"}, 3000);
+    $(".fontSubTitleBody").animate({marginTop: "2000px"}, 3000);
+    $(".pdf").animate({marginTop: "2000px"}, 3000);
+    $(".fontRegular").animate({marginTop: "2000px"}, 3000);
+    $(".headshot").animate({marginLeft: "-1000px"}, 3000);
+    $(".skillsIcon").animate({marginTop: "2000px"}, 3000);
+    $(".skillsFont").fadeOut(5000);
+    $("#socialIcon1").animate({marginLeft: "-1000px"}, 3000);
+    $("#socialIcon2").animate({marginLeft: "-1000px"}, 3000);
+    $("#socialIcon3").animate({marginLeft: "-1000px"}, 3000);
+    $("#socialIcon4").animate({marginLeft: "-1000px"}, 3000);
+    setTimeout(function(){
+        window.location = "./resume.html"
+    }, 3000);
+  });
 
+  $(navIcon3).click(function(){
+    $("#bgvid").fadeOut(3000);
+    $("#navIcon1").fadeOut(3000);
+    $("#navIcon2").fadeOut(3000);
+    $("#navIcon3").fadeOut(3000);
+    $("#navIcon4").fadeOut(3000);
+    $(".fontTitle").animate({marginLeft: "-1000px"}, 3000);
+    $(".fontSubTitleLeft").animate({marginLeft: "-1000px"}, 3000);
+    $(".fontSubTitleBody").animate({marginTop: "2000px"}, 3000);
+    $(".pdf").animate({marginTop: "2000px"}, 3000);
+    $(".fontRegular").animate({marginTop: "2000px"}, 3000);
+    $(".headshot").animate({marginLeft: "-1000px"}, 3000);
+    $(".skillsIcon").animate({marginTop: "2000px"}, 3000);
+    $(".skillsFont").fadeOut(5000);
+    $("#socialIcon1").animate({marginLeft: "-1000px"}, 3000);
+    $("#socialIcon2").animate({marginLeft: "-1000px"}, 3000);
+    $("#socialIcon3").animate({marginLeft: "-1000px"}, 3000);
+    $("#socialIcon4").animate({marginLeft: "-1000px"}, 3000);
+    setTimeout(function(){
+        window.location = "./work.html"
+    }, 3000);
+  });
+
+  $(navIcon4).click(function(){
+    $("#bgvid").fadeOut(3000);
+    $("#navIcon1").fadeOut(3000);
+    $("#navIcon2").fadeOut(3000);
+    $("#navIcon3").fadeOut(3000);
+    $("#navIcon4").fadeOut(3000);
+    $(".fontTitle").animate({marginLeft: "-1000px"}, 3000);
+    $(".fontSubTitleLeft").animate({marginLeft: "-1000px"}, 3000);
+    $(".fontSubTitleBody").animate({marginTop: "2000px"}, 3000);
+    $(".pdf").animate({marginTop: "2000px"}, 3000);
+    $(".fontRegular").animate({marginTop: "2000px"}, 3000);
+    $(".headshot").animate({marginLeft: "-1000px"}, 3000);
+    $(".skillsIcon").animate({marginTop: "2000px"}, 3000);
+    $(".skillsFont").fadeOut(5000);
+    $("#socialIcon1").animate({marginLeft: "-1000px"}, 3000);
+    $("#socialIcon2").animate({marginLeft: "-1000px"}, 3000);
+    $("#socialIcon3").animate({marginLeft: "-1000px"}, 3000);
+    $("#socialIcon4").animate({marginLeft: "-1000px"}, 3000);
+    setTimeout(function(){
+        window.location = "./contact.html"
+    }, 3000);
+  });
