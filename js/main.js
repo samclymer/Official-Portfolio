a27cfc690c9a55362ec33f2ed89cbbb1f9883ae4
Sam C <samclymer@gmail.com>
2019-03-06 13:21:40 -0500

    currntly working on final animations

--- js/main.js
+++ js/main.js
@@ -1,14 +1,39 @@
 /* Fade in Navigation Icons upon page load*/
 $(document).ready(function(){
-      $("#bgvid").fadeIn(2000);
-      $("#navIcon1").fadeIn(2000);
-      $("#navIcon2").fadeIn(2500);
+      $("#bgvid").fadeIn(3000);
+      $("#navIcon1").fadeIn(3000);
+      $("#navIcon2").fadeIn(3000);
       $("#navIcon3").fadeIn(3000);
-      $("#navIcon4").fadeIn(3500);
-      $("#navIcon5").fadeIn(4000);
-      $(".fontTitle").fadeIn(3000);
-      $(".fontSubTitle").fadeIn(3500);
-      $(".fontRegular").fadeIn(3500);
-      $(".headshot").fadeIn(4000);
+      $("#navIcon4").fadeIn(3000);
+      $(".fontTitle").animate({marginLeft: "0px"}, 3000);
+      $(".fontSubTitleLeft").animate({marginLeft: "0px"}, 3000);
+      $(".fontRegular").animate({marginBottom: "0px"}, 3000);
+      $(".headshot").animate({marginLeft: "0px"}, 3000);
+      $(".skillsIcon").animate({marginTop: "0px"}, 3000);
+      $(".skillsFont").fadeIn(5000);
+      $("#socialIcon1").animate({marginLeft: "0px"}, 3000);
+      $("#socialIcon2").animate({marginLeft: "0px"}, 3000);
+      $("#socialIcon3").animate({marginLeft: "0px"}, 3000);
+      $("#socialIcon4").animate({marginLeft: "0px"}, 3000);
+});
+
+  $(navIcon1).click(function(){
+    $("#bgvid").fadeOut(2000);
+    $("#navIcon1").fadeOut(2000);
+    $("#navIcon2").fadeOut(2000);
+    $("#navIcon3").fadeOut(2000);
+    $("#navIcon4").fadeOut(2000);
+    $(".fontTitle").animate({marginLeft: "-1000px"}, 1000);
+    $(".fontSubTitleLeft").animate({marginLeft: "-1000px"}, 1500);
+    $(".fontRegular").fadeOut(3500);
+    $(".headshot").animate({marginLeft: "-1000px"}, 2000);
+    $(".skillsIcon").fadeOut(4000);
+    $(".skillsFont").fadeOut(4000);
+    $(".socialIcon").fadeOut(4000);
+    setTimeout(function(){
+        window.location = "./index.html"
+    }, 4500);
+    return false;
   });
+
 
