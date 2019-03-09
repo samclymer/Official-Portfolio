ab440067d1ff7c712d45c6ad46bd313d4981f1aa
Sam C <samclymer@gmail.com>
2019-03-09 03:18:03 -0500

    added in different php code as last one did not work

--- mail.php
+++ mail.php
@@ -1,10 +1,24 @@
-<?php $name = $_POST['name'];
-$email = $_POST['email'];
+<?php 
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
 $message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "samclymer@gmail.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+
+$email_from = "samclymer@gmail.com";
+
+$email_subject = "New Form Submission";
+
+$email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                  "User Message: $message.\n";
+
+$to = "samclymer@gmail.com";
+
+$headers = "From: $email_from \r\n";
+
+$headers = "Reply-to: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+
 ?>
